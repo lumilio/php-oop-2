1b38218694233ba407876e414346d788024abac9
lumilio <logistic-division@lumilium.com>
2022-01-10 02:34:30 +0100

    #

--- index.php
+++ index.php
@@ -15,12 +15,27 @@ require_once __DIR__ . '/Classi/product_prime.php';
 $prodotto1 = new Product('Aciugacapelli', 50.00, 'asciuga i capelli');
 $prodotto2 = new Product('Fermaporta', 10.99, 'trattiene la porta');
 $prodotto3 = new Product('Borraccia', 20.50, 'contenitore per bevande');
+
 $user1 = new User('Marco', 'Rossi', '30', 'marcorossi@gmail.com');
 $user2 = new User('Dario', 'Bianchi', '50', 'dariobianchi@gmail.com');
 $user3 = new User('Luigi', 'Verdi', '20', 'luigiverdi@gmail.com');
+
 $user1_creditcard = new Creditcard('1234 5678 1234 5678', '000', '17061998', 'Mastercard');
 $user2_creditcard = new Creditcard('0000 5678 1234 5678', '679', '10111962', 'Visa');
 $user3_creditcard = new Creditcard('1234 5678 0000 5678', '888', '03052000', 'Mastercard');
+
+
+
+
+var_dump($prodotto1);
+var_dump($prodotto2);
+var_dump($prodotto3);
+var_dump($user1);
+var_dump($user2);
+var_dump($user3);
+var_dump($user1_creditcard);
+var_dump($user2_creditcard);
+var_dump($user3_creditcard);
 
 
 
